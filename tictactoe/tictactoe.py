f93e1dd16b4873299a52d310625dbe706aacb67a
beomjin-lee <cluesbj@gmail.com>
2018-10-04 22:15:56 -0700

    initial commit of tictactoe

---
+++ tictactoe/tictactoe.py
@@ -0,0 +1,32 @@
+def vertical_check(board):
+    ## TODO:
+
+def horizontal_check(board):
+    ## # TODO:
+
+def diagonal_check(board):
+    # # TODO:
+
+def draw_check(board):
+    # TODO:
+
+def main():
+    board = {
+        (0, 0) : 0,
+        (0, 1) : 0,
+        (0, 2) : 0,
+        (1, 0) : 0,
+        (1, 1) : 0,
+        (1, 2) : 0,
+        (2, 0) : 0,
+        (2, 1) : 0,
+        (2, 2) : 0
+    }
+
+    step = 0
+
+
+
+
+if __name__ == "__main__":
+    main()
