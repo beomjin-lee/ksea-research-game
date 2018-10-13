225905baa8d2ed8a88e1b38f13711ec0a8e9b031
beomjin-lee <cluesbj@gmail.com>
2018-10-12 17:16:26 -0700

    initial commit of tetris

---
+++ tetris.py
@@ -0,0 +1,31 @@
+class Board:
+    def __init__(self, size):
+        # Put the board implementation here
+        # Hashmap? Array? Future of KSEA lies in your hands
+        self.size = size
+        self.is_piece_set = False # piece moving
+
+    def game_over(self):
+
+    def change_is_piece_set(self):
+
+
+
+class Piece:
+    def __init__(self):
+
+    def random_piece_gen(self):
+
+
+class Game:
+    def __init__(self):
+        self.score = 0
+
+    def timer(self):
+
+    def move(self):
+
+
+    def rotate(self):
+
+    def score(self):
