7e48b0a0ba4c1a009a6be71c3d901de6db0e7d46
Gabi Choi <gabichoi@airbears2-10-142-108-85.airbears2.1918.berkeley.edu>
2018-10-19 18:45:57 -0700

    subclasses of pieces

--- tetris.py
+++ tetris.py
@@ -11,16 +11,74 @@ class Board:
         # Brian L.
 
     def change_is_piece_set(self):
+        print("Hello")
         # Wonwoo
 
 
 
 class Piece:
+    # Shape names reference: https://tetris.wiki/Tetromino
     def __init__(self):
         #
+        cleared_row_count = 0
+        dimension = [0][0]
+        position = (0, 0)
+
+    class I_shape:
+        dimension = [1][4]
+        position = 
+        dimension[0][0] = 1 
+        dimension[0][1] = 1
+        dimension[0][2] = 1
+        dimension[0][3] = 1
+
+    class O_shape:
+        dimension = [2][2]
+        position = 
+        dimension[0][0] = 1 
+        dimension[0][1] = 1
+        dimension[1][0] = 1
+        dimension[1][1] = 1
+    
+    class T_shape:
+        dimension = [2][3]
+        position = 
+        dimension[0][0] = 1 
+        dimension[0][1] = 1
+        dimension[1][0] = 1
+        dimension[1][1] = 1
+        dimension[1][0] = 1
+        dimension[1][1] = 1
+    
+    class S_shape:
+        dimension = [2][3]
+        position = 
+    
+    class Z_shape:
+        dimension = [2][3]
+        position = 
+    
+    class J_shape:
+        dimension = [2][3]
+        position = 
+    
+    class L_shpae:
+        dimension = [2][3]
+        position = 
+
+    
+
+
 
     def random_piece_gen(self):
-        #
+        
+
+    def rotate_piece(self):
+
+
+    def change_row(self):
+        row_num = row_num - cleared_row_count
+
 
 
 class Game:
